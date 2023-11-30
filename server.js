c393434ef83ffb4165d6857ed5d89e341d8ef7c4
Russell Montjoy <russellmontjoy@gmail.com>
2023-11-29 19:34:20 -0700

    update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express'); 
-const mysql = require('mysql'); 
+
 
 const PORT = process.env.PORT || 3001; 
 const app = express (); 
@@ -8,15 +8,3 @@ const app = express ();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // TODO: Add MySQL password here
-    password: 'root123',
-    database: 'satellite_db',
-  },
-  console.log(`Connected to the Satellite_db database.`)
-);
