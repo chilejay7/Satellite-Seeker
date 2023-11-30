c6f03a82c57a9942760f4d4cbc65052b6152aa35
Brandon Wing <brandon.wing245@gmail.com>
2023-11-29 21:07:47 -0700

    Fixed User model brackets for sequelize

--- models/User.js
+++ models/User.js
@@ -37,12 +37,10 @@ User.init(
   {
     hooks: {
       beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUser.password, 10);
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
     },
-  },
-  {
     sequelize,
     timestamps: false,
     freezerTableName: true,
