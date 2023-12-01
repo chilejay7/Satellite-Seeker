b3a031b4b65033fd6fd5db2ec9d3b506e346030f
chilejay7 <codyburk7@yahoo.com>
2023-11-30 18:39:01 -0700

    models updated

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const loginRoutes = require('./login');
 
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 router.use('/login', loginRoutes);
 
 module.exports = router;
