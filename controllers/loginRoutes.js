7b3c306c34fdaba35c61098a9c65e071bba86878
chilejay7 <codyburk7@yahoo.com>
2023-11-30 19:41:31 -0700

    login route updated

--- controllers/loginRoutes.js
+++ controllers/loginRoutes.js
@@ -7,7 +7,21 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     console.log(`Username is: ${req.body.user_name} and the password is: ${req.body.password}`);
-    const newUser = await User.create();
+
+    const { user_name, password } = req.body;
+
+
+    const userData = await User.findOne({
+        where: {
+            user_name: user_name.toLowerCase(),
+        }
+    });
+
+    if (!userData) {
+        res.status(400).render('login');
+        return;
+    }
+
 });
 
 module.exports = router;
