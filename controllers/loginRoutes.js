56db5273cdee1364ac81bd4a0e84b5962e6a6711
chilejay7 <codyburk7@yahoo.com>
2023-11-30 19:34:43 -0700

    adds login GET route and handlebars view

--- controllers/loginRoutes.js
+++ controllers/loginRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 
 router.get('/', async (req, res) => {
-    res.send('Login page is here');
+    res.render('login');
 })
 
 router.post('/', async (req, res) => {
