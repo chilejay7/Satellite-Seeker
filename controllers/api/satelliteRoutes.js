9d2f73b6e554c48bd48ff6cf7b74b7659ac21ad5
chilejay7 <codyburk7@yahoo.com>
2023-12-03 14:37:34 -0700

    passes loggedin value to views

--- controllers/api/satelliteRoutes.js
+++ controllers/api/satelliteRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('sat');
+    res.render('sat', {
+        loggedIn: req.session.loggedIn,
+    });
 });
 
 module.exports = router;
