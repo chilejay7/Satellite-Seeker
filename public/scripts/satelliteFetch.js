5239b21adb9c6e9216475b140c07ed1bc9712f90
chilejay7 <codyburk7@yahoo.com>
2023-12-03 18:33:53 -0700

    satellite search form values captured through script file

--- public/scripts/satelliteFetch.js
+++ public/scripts/satelliteFetch.js
@@ -2,7 +2,15 @@ const satSearchForm = document.getElementById('satSearchForm');
 
 const searchData = (e) => {
     e.preventDefault();
-    console.log(e);
+
+    // These have to be defined within the code block of the function otherwise they'll be read as having an empty value if defined outside of the function.
+    const satelliteName = document.getElementById('satellite_name').value;
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+    
+    console.log(satelliteName);
+    console.log(latitude);
+    console.log(longitude);
 };
 
 satSearchForm.addEventListener('submit', searchData);
