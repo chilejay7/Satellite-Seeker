1bdd352b9cc2c9f4692ee846ce8cb44b81d29cae
Brandon Wing <brandon.wing245@gmail.com>
2023-12-05 22:31:33 -0700

    Added coordinates to satellite seed data, also formatting css and route

--- seeds/satelliteSeeds.js
+++ seeds/satelliteSeeds.js
@@ -5,26 +5,36 @@ const satelliteData = [
     satellite_name: 'XAIOXIANG-1 07',
     country_id: 4,
     norad_id: 44519,
+    latitude: 22.39,
+    longitude: 17.63,
   },
   {
     satellite_name: 'TELSTAR 18V',
     country_id: 3,
     norad_id: 43611,
+    latitude: -14.59,
+    longitude: -28.67,
   },
   {
     satellite_name: 'GEI-KOMPSAT-2B',
     country_id: 2,
     norad_id: 45246,
+    latitude: 27.98,
+    longitude: 86.92,
   },
   {
     satellite_name: 'SENTINEL 2B',
     country_id: 1,
     norad_id: 42063,
+    latitude: 51.17,
+    longitude: 10.42,
   },
   {
     satellite_name: 'SKYSAT 2',
     country_id: 1,
     norad_id: 40072,
+    latitude: 40.71,
+    longitude: -74,
   },
 ];
 
